521879f894b0cb3f82ac3822215d03392bbd8b66
dmsquf4061 <dmsquf4061@naver.com>
2025-11-26 19:43:31 +0900

    비디오 수정

--- src/components/pages/landing/section/video.jsx
+++ src/components/pages/landing/section/video.jsx
@@ -1,8 +1,27 @@
+import ReactPlayer from 'react-player';
+
 function VideoPage() {
   return (
-    <section className="flex flex-col justify-center items-center w-full h-[380px] md:h-[1000px] gap-10 md:gap-15 px-5 py-10 md:py-20">
+    <section className="flex flex-col justify-center items-center w-full gap-10 md:gap-15 px-5 py-10 md:py-20">
         <div className="flex flex-col w-full md:max-w-9/10 h-full gap-8 items-center bg-stone-500 rounded-md md:rounded-2xl">
-          <video  src=""></video>
+          {/* <video 
+            className="w-full"
+            src="/resource/mainhero/sample_video.mp4"
+            autoPlay
+            muted
+            controls={false}
+            loop
+          ></video> */}
+          <ReactPlayer 
+            className="w-full"
+            width="100%"
+            height="100%"
+            src="/resource/mainhero/sample_video.mp4"
+            autoPlay
+            muted
+            controls
+            loop
+          />
         </div>
     </section>
   )
